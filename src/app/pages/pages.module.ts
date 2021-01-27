db1a57022321d39180463a2fb9798558ad97cc26
Jose Guadalupe Reyes Lopez <pepereyes07@hotmail.com>
2021-01-27 12:11:24 -0600

    publicar cambios

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { PAGES_ROUTES } from './pages.routes';
+
+import { ProgressComponent } from './progress/progress.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    PagesComponent,
+  ],
+  exports: [DashboardComponent, ProgressComponent, Graficas1Component],
+  imports: [SharedModule, PAGES_ROUTES],
+})
+export class PagesModule {}
